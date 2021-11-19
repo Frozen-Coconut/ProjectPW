34a305a7a989d8c41c7769b272b044894ab819f8
leonard3368 <51233664+leonard3368@users.noreply.github.com>
2021-11-19 18:00:47 +0800

    update home page

---
+++ home.php
@@ -0,0 +1,102 @@
+<?php require_once("./util/docOpen.php") ?>
+
+<?php require_once("./util/navbar.php")?>
+
+
+<section class="w-full h-screen bg-center bg-no-repeat bg-cover flex" style="background-image: url('https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80');">
+    <div class="w-full h-screen bg-opacity-70 bg-black flex justify-center items-center">
+        <div class="w-1/2"></div>
+        <div class="w-1/2">
+        <div class="mx-4 text-left text-white px-10">
+            <h1 class="font-bold text-6xl mb-4">TokoKita</h1>
+            <p class="text-1xl mb-4">Toko alat musik terlengkap di Indonesia! Kami menyediakan alat musik professional dan pro audio dari merek ternama.</p>
+            <div>
+                <input type="text" class="rounded-md px-7    py-3 transition duration-300 ease-in-out text-black" placeholder="Telusuri Toko">
+                <button href="" class="bg-red-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-red-600 ml-2">
+                    Search
+                </button>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
+<section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+    <div class="text-center pb-12">
+        <h2 class="text-base font-bold text-indigo-600">
+            Bingung mulai darimana?
+        </h2>
+        <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+            Lihat Kategori Kami
+        </h1>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="w-full bg-white p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover h-36 w-36" src="https://cdn.shopify.com/s/files/1/0117/8740/3323/products/F03-014-9933-515_1545619811889.jpg?v=1557222725" alt="photo">
+            </div>
+            <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">instrument</p>
+            </div>
+        </div>
+        <div class="w-full bg-white p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover h-36 w-36" src="https://cdn.shopify.com/s/files/1/0117/8740/3323/products/F03-014-9933-515_1545619811889.jpg?v=1557222725" alt="photo">
+            </div>
+            <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">instrument</p>
+            </div>
+        </div>
+        <div class="w-full bg-white-lg p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover h-36 w-36" src="https://cdn.shopify.com/s/files/1/0117/8740/3323/products/F03-014-9933-515_1545619811889.jpg?v=1557222725" alt="photo">
+            </div>
+            <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">instrument</p>
+            </div>
+        </div><div class="w-full bg-white p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover h-36 w-36" src="https://cdn.shopify.com/s/files/1/0117/8740/3323/products/F03-014-9933-515_1545619811889.jpg?v=1557222725" alt="photo">
+            </div>
+            <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">instrument</p>
+            </div>
+        </div>
+        <div class="w-full bg-white p-12 flex flex-col justify-center items-center">
+            <div class="mb-8">
+                <img class="object-center object-cover h-36 w-36" src="https://cdn.shopify.com/s/files/1/0117/8740/3323/products/F03-014-9933-515_1545619811889.jpg?v=1557222725" alt="photo">
+            </div>
+            <div class="text-center">
+                <p class="text-xl text-gray-700 font-bold mb-2">instrument</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="w-full h-screen bg-center bg-no-repeat bg-cover flex" style="background-image: url('https://images.unsplash.com/photo-1620812097331-ff636155488f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80');">
+    <div class="w-full h-screen bg-opacity-70 bg-black flex justify-center items-center">
+            <div class="w-1/2">
+                <div class="mx-4 text-left text-white px-10">
+                    <h2 class="font-bold text-3xl mb-4">New Release</h2>
+                    <h1 class="font-bold text-6xl mb-4">Kawaii K-300 48"</h1>
+                    <p class="text-1xl mb-4">K-300 dibangun di atas keberhasilan pendahulunya yang memenangkan penghargaan, K-3, yang dinobatkan sebagai "Acoustic Piano of the Year" dari Majalah MMR selama empat tahun berturut-turut. K-300, dengan bangga meneruskan warisan yang luar biasa ini. </p>
+                    <button href="" class="text-lg transition-all text-gray-100 ring-2 px-4 py-0.5 rounded ring-white hover:bg-white hover:text-gray-500">
+                        Buy Now
+                    </button>
+                </div>
+            </div>
+            <div class="w-1/3">
+                <img class ="shadow-inner" src="https://kawaius.com/wp-content/uploads/2018/04/K-300-Polished-Ebony-600x600.jpg" alt="Image of Kawaii K-300">
+            </div>    
+    </div>
+</section>
+<section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+<div class="text-center pb-12">
+        <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+            Our Partners
+        </h1>
+    </div>
+</section>
+
+
+
+<?php require_once("./util/footer.php") ?>
+<?php require_once("./util/docClose.php") ?>
