bb5b3e1f75247e80f80f7fd2ee52e627bb99ebdb
mikhaelc09 <82882832+mikhaelc09@users.noreply.github.com>
2021-11-22 19:51:12 +0700

    Update colors.php

--- util/colors.php
+++ util/colors.php
@@ -12,19 +12,19 @@
 </head>
 <body>
     <div class="flex justify-around flex-wrap">
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-white">hh-white</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-black-dark text-white">hh-black-dark</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-black-light text-white">hh-black-light</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-orange-darker">hh-orange-dark</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-orange-dark">hh-orange-dark</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-orange-light">hh-orange-light</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-orange-lighter">hh-orange-light</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-pink-darker">hh-pink-dark</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-pink-dark">hh-pink-dark</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-pink-light">hh-pink-light</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-pink-lighter">hh-pink-light</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-gray-dark">hh-gray-dark</div>
-        <div class="w-32 h-24 text-xl font-bold border-2 border-black bg-hh-gray-light">hh-gray-light</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-black-dark text-white">hh-black-dark</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-black-light text-white">hh-black-light</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-gray-dark">hh-gray-dark</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-gray-light">hh-gray-light</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-white">hh-white</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-orange-darker">hh-orange-dark</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-orange-dark">hh-orange-dark</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-orange-light">hh-orange-light</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-orange-lighter">hh-orange-light</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-pink-darker">hh-pink-dark</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-pink-dark">hh-pink-dark</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-pink-light">hh-pink-light</div>
+        <div class="w-28 h-24 text-xl font-bold border-2 border-black bg-hh-pink-lighter">hh-pink-light</div>
     </div>
 </body>
 </html>
