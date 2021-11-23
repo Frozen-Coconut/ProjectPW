45d01f74dc7d7dd87bb97d2dd895b1d539ad50e3
leonard3368 <51233664+leonard3368@users.noreply.github.com>
2021-11-23 11:46:49 +0800

    Create wishlist.php

---
+++ wishlist.php
@@ -0,0 +1,26 @@
+<?php require_once("./util/docOpen.php") ?>
+
+<?php require_once("./util/navbar.php")?>
+
+<section class="w-full min-h-screen">
+    <h1 class="text-3xl px-7 py-4">WishList</h1>
+    <table class="w-full text-left ">
+        <thead>
+            <tr class="border-b">
+                <th class="w-1/3 py-4 px-7">Product</th>
+                <th class="w-1/3 py-4 px-7">Price</th>
+                <th class="w-1/3 py-4 px-7">Remove</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-b">
+                <td class="py-4 px-7"><img src="" alt=""></td>
+                <td class="py-4 px-7">test</td>
+                <td class="py-4 px-7"> </td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+
+<?php require_once("./util/footer.php") ?>
+<?php require_once("./util/docClose.php") ?>
