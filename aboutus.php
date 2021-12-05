9b1b4029965bf8131334263ea2e7e777372f45ab
mikhaelc09 <82882832+mikhaelc09@users.noreply.github.com>
2021-12-05 11:28:27 +0700

    about us update

--- aboutus.php
+++ aboutus.php
@@ -5,30 +5,46 @@
     <script>
         // document.title = 'About Us';
     </script>
-    <div class="container min-h-screen flex flex-col justify-evenly items-center">
+    <div class="container min-h-screen flex flex-col justify-evenly items-center mx-auto">
         <div class="w-3/4 mt-8 mb-3 flex flex-col">
             <div class="text-hh-orange-dark text-4xl font-bold">Tim Kami</div>
             <div class="text-gray-600 text-xl mt-5">Kelompok ini merupakan kelompok yang terdiri atas 4 mahasiswa Institut Sains & Teknologi Terpadu Surabaya (iSTTS) , yang terbentuk untuk mengerjakan proyek yang diberikan sebagai tugas Mata Kuliah Pengembangan Aplikasi Internet. Website ini merupakan website non profit dan hanya bertujuan sebagai sarana kami untuk belajar mengembangan sebuah online shop.</div>
         </div>
-        <div class="w-3/4 my-mt-3 mb-8 flex">
-            <div>
+        <div class="w-5/6 mt-3 mb-8 flex">
+            <div class="w-full ">
                 <div class="flex items-center my-5 card">
-                    <div class="w-40 h-56 bg-gray-300 shadow-lg rounded-lg"></div>
-                    <div class="w-2/3 ml-3 transition-all">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, in at? Ullam, architecto. Perferendis, officiis aliquam. Amet maiores aspernatur ab consequuntur error quaerat obcaecati eum facilis, iste vitae? Quo, dolores.</div>
+                    <div class="w-40 h-56 bg-cover shadow-lg rounded-lg bg-center" style="background-image: url('');"></div>
+                    <div class="w-2/3 ml-3 transition-all flex flex-col">
+                        <span>Nama Lengkap : </span>
+                        <span>NRP : </span>
+                        <span>Contacts : </span>
+                    </div>
                 </div>
                 <div class="flex items-center my-5 card">
-                    <div class="w-40 h-56 bg-gray-300 shadow-lg rounded-lg"></div>
-                    <div class="w-2/3 ml-3 transition-all">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, in at? Ullam, architecto. Perferendis, officiis aliquam. Amet maiores aspernatur ab consequuntur error quaerat obcaecati eum facilis, iste vitae? Quo, dolores.</div>
+                    <div class="w-40 h-56 bg-cover shadow-lg rounded-lg bg-center" style="background-image: url('https://upload.wikimedia.org/wikipedia/commons/b/be/Pygoscelis_papua.jpg');"></div>
+                    <div class="w-2/3 ml-3 transition-all flex flex-col">
+                        <span>Nama Lengkap : Hartoyo Lijaya</span>
+                        <span>NRP : 220116874</span>
+                        <span>Contacts : <a href="https://www.instagram.com/hartoyo_lijaya/">Instagram</a></span>
+                    </div>
                 </div>
             </div>
-            <div>
+            <div class="w-full ">
                 <div class="flex items-center my-5 card">
-                    <div class="w-40 h-56 bg-gray-300 shadow-lg rounded-lg"></div>
-                    <div class="w-2/3 ml-3 transition-all">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, in at? Ullam, architecto. Perferendis, officiis aliquam. Amet maiores aspernatur ab consequuntur error quaerat obcaecati eum facilis, iste vitae? Quo, dolores.</div>
+                    <div class="w-40 h-56 bg-cover shadow-lg rounded-lg bg-center" style="background-image: url('');"></div>
+                    <div class="w-2/3 ml-3 transition-all flex flex-col">
+                        <span>Nama Lengkap : </span>
+                        <span>NRP : </span>
+                        <span>Contacts : </span>
+                    </div>
                 </div>
                 <div class="flex items-center my-5 card">
-                    <div class="w-40 h-56 bg-gray-300 shadow-lg rounded-lg"></div>
-                    <div class="w-2/3 ml-3 transition-all">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta, in at? Ullam, architecto. Perferendis, officiis aliquam. Amet maiores aspernatur ab consequuntur error quaerat obcaecati eum facilis, iste vitae? Quo, dolores.</div>
+                    <div class="w-40 h-56 bg-cover shadow-lg rounded-lg bg-center" style="background-image: url('https://pokemongohub.net/wp-content/uploads/2021/02/Dtyz5JwUwAAqvxi.jpg');"></div>
+                    <div class="w-2/3 ml-3 transition-all flex flex-col">
+                        <span>Nama Lengkap : Mikhael Chris</span>
+                        <span>NRP : 220116890</span>
+                        <span>Contacts : <a href="https://www.instagram.com/mikhael_09/">Instagram</a> <a href="https://www.linkedin.com/in/mikhael-chris/">LinkedIn</a> <a href="https://github.com/mikhaelc09">Github</a></span>
+                    </div>
                 </div>
             </div>
         </div>
