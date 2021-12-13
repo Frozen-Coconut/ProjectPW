a95c40b273421acd8764477739a95858acefecd0
mikhaelc09 <82882832+mikhaelc09@users.noreply.github.com>
2021-12-13 16:50:00 +0700

    update dikit

--- login.php
+++ login.php
@@ -45,7 +45,7 @@
         // document.title = 'Login';
     </script>
     <div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-hh-orange-light to-hh-pink-light">
-        <a href="index.php"><button class="absolute left-4 top-4 px-5 py-2 bg-gray-400 hover:opacity-80 shadow-md">Kembali</button></a>
+        <a href="index.php"><button class="absolute left-4 top-4 px-5 py-2 bg-hh-pink-dark text-white hover:opacity-80 shadow-md rounded">Kembali</button></a>
         <div class="border-2 border-black border-opacity-25 rounded-lg flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-hh-white bg-opacity-50">
             <div class="max-w-md w-full space-y-8">
                 <div>
